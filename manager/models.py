8c51bfbcbe1ad68a3a4e9d98c91ca6e4fb468be6
Anurag Sharma <anurag.asy81@gmail.com>
2021-06-14 00:57:02 +0530

    first commit

---
+++ manager/models.py
@@ -0,0 +1,53 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Members(db.Model):
+    memID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    phone = db.Column(db.String(150))
+    email = db.Column(db.String(150))
+    fine = db.Column(db.String(150))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    trans = db.relationship('Trans',backref='transfor')
+  
+
+class Books(db.Model):
+    bookID = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    authors = db.Column(db.String(150))
+    isbn = db.Column(db.String(150))
+    publisher = db.Column(db.String(150))
+    num_pages = db.Column(db.String(150))
+    stock = db.Column(db.String(150))
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    trans = db.relationship('Trans',backref='transby')
+
+
+
+class Trans(db.Model):
+    transID = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.bookID'))
+    mem_id = db.Column(db.Integer, db.ForeignKey('members.memID'))
+    
+    member_name = db.Column(db.String(150))
+    book_name = db.Column(db.String(150))
+    iss_date = db.Column(db.String(150))
+    ret_date = db.Column(db.String(150))
+    rent = db.Column(db.String(150))
+    fine = db.Column(db.String(150))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    Books = db.relationship('Books')
+    Members = db.relationship('Members')
+    Trans = db.relationship('Trans')
---
+++ manager/views.py
@@ -0,0 +1,95 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+
+import json
+
+from .models import Books,Members,Trans
+from . import db
+
+
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():  
+    # result = Books.query.filter_by(bookID=1).first()
+    # print(result.user_id)
+    if request.method == 'POST':
+        title = request.form.get('title')
+        authors = request.form.get('authors')
+        isbn = request.form.get('isbn')
+        publisher = request.form.get('publisher')
+        num_pages = request.form.get('num_pages')
+        stock = request.form.get('stock')
+        data = request.form.get('data')
+
+        if len(title) < 1:
+            flash('title is too short!', category='error')
+        else:
+            new_Book = Books(title=title, authors=authors, isbn=isbn,publisher=publisher,num_pages=num_pages,stock=stock,data=data, user_id=current_user.id)
+            db.session.add(new_Book)
+            db.session.commit()
+            flash('Added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+
+
+@views.route('/members', methods=['GET', 'POST'])
+@login_required
+def members():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        phone = request.form.get('phone')
+        email = request.form.get('email')
+        fine = request.form.get('fine')
+
+        if len(name) < 1:
+            flash('name is too short!', category='error')
+        else:
+            new_member = Members(name=name, phone=phone, email=email,fine=fine, user_id=current_user.id)
+            db.session.add(new_member)
+            db.session.commit()
+            flash('Added!', category='success')
+
+    return render_template("members.html", user=current_user)
+
+
+@views.route('/trans', methods=['GET', 'POST'])
+@login_required
+def trans():
+    books= Books.query.all()
+    trans= Trans.query.all()
+    members= Members.query.all()
+    
+
+    if request.method == 'POST':
+        book_name = request.form.get('book_name')
+        mem_name = request.form.get('member_name')
+        iss_date = request.form.get('iss_date')
+        ret_date = request.form.get('ret_date')
+        rent = request.form.get('rent')
+        fine = request.form.get('fine')
+       
+
+        mem_id=Members.query.filter_by(name=mem_name).first()
+        book_id=Books.query.filter_by(title=book_name).first()
+      
+        if len(iss_date) < 1:
+            flash('name is too short!', category='error')
+       
+        else:
+            new_trans = Trans(transby=book_id, transfor= mem_id, member_name=mem_name ,book_name=book_name ,iss_date=iss_date,ret_date=ret_date,rent = rent,fine = fine , user_id=current_user.id)
+            db.session.add(new_trans)
+            db.session.commit()
+            flash('Added!', category='success')
+
+        
+
+    return render_template("trans.html", user=current_user,books=books, trans=trans,members=members)
+
+
+
+    
